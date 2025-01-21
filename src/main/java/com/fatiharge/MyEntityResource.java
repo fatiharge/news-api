2f4f5c8be70aa9c43caa01f1c3713a5bc3986c68
quarkusio <no-reply@quarkus.io>
2025-01-21 07:03:10 +0000

    Initial commit

---
+++ src/main/java/com/fatiharge/MyEntityResource.java
@@ -0,0 +1,6 @@
+package com.fatiharge;
+
+import io.quarkus.hibernate.orm.rest.data.panache.PanacheEntityResource;
+
+public interface MyEntityResource extends PanacheEntityResource<MyEntity, Long> {
+}
